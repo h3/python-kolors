24b7d0483a04e2f140eef826d6c60829c182b76f
Maxime Haineault <haineault@gmail.com>
2017-07-27 04:40:16 -0400

    Initial import

---
+++ setup.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+
+import os
+
+from setuptools import setup, find_packages
+
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name='kolors',
+    version='0.0.0',
+    description='Simple and lightweight shell color output function.',
+    long_description=(read('README.md')),
+    author='Maxime Haineault',
+    author_email='haineault@gmail.com',
+    license='MIT',
+    url='https://github.com/h3/django-pdfutils',
+    packages=find_packages(),
+    include_package_data=True,
+    package_data={'kolors': [
+        'README.rst',
+        'CHANGELOG',
+        ]},
+    zip_safe=True,
+    classifiers=[
+        'Environment :: Shell',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ]
+)
